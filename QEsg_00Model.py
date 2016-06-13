a969925a7ce62d5c9bd147e34dd0cf238c23e6f2
jorgealmerio <jorgealmerio@gmail.com>
2016-06-13 09:29:02 -0300

    Update QEsg_00Model.py

--- QEsg_00Model.py
+++ QEsg_00Model.py
@@ -20,7 +20,7 @@
 # write to the Free Software Foundation, Inc., 59 Temple Place
 # - Suite 330, Boston, MA 02111-1307, USA.
 #
-
+#teste de edição
 # Describe the model structure
 from PyQt4.QtCore import *
 
